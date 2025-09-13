b8208b43426dafd19010a961fe5933028d8d7e32
schrolan <schrolan000@gmail.com>
2025-09-12 19:31:38 -0500

    added some hover effects

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,59 +1,47 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useRef } from "react";
 import Slider from "react-slick";
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate, useLocation } from "react-router-dom";
 
-import About from '../pages/aboutMe';
-import Projects from '../pages/projects';
-import Certificate from '../pages/certificate';
-import Contact from '../pages/contact';
+import About from "../pages/aboutMe";
+import Projects from "../pages/projects";
+import Certificate from "../pages/certificate";
+import Contact from "../pages/contact";
 
-import '../index.css';
+import "../index.css";
 
 const slides = [
-  { path: '/', component: <About /> },
-  { path: '/projects', component: <Projects /> },
-  { path: '/certificate', component: <Certificate /> },
-  { path: '/contact', component: <Contact /> }
+  { path: "/", component: <About /> },
+  { path: "/projects", component: <Projects /> },
+  { path: "/certificate", component: <Certificate /> },
+  { path: "/contact", component: <Contact /> }
 ];
 
 const Carousel = () => {
+  const sliderRef = useRef(null);
   const navigate = useNavigate();
   const location = useLocation();
-  const sliderRef = useRef(null);
-
-  const currentIndex = slides.findIndex(slide => slide.path === location.pathname);
-
-  const handleAfterChange = (index) => {
-    const newPath = slides[index].path;
-    if (newPath !== location.pathname) {
-      navigate(newPath);
-    }
-  };
-
-  useEffect(() => {
-    if (sliderRef.current && currentIndex !== sliderRef.current.innerSlider.state.currentSlide) {
-      sliderRef.current.slickGoTo(currentIndex);
-    }
-  }, [currentIndex]);
 
   const settings = {
     dots: true,
-    infinite: false,
-    speed: 500,
-    swipeToSlide: true,
+    infinite: true,
+    speed: 800,
     slidesToShow: 1,
     slidesToScroll: 1,
-    arrows: true,
-    afterChange: handleAfterChange,
-    adaptiveHeight: false
+    adaptiveHeight: true,
+    afterChange: (current) => {
+      const newPath = slides[current].path;
+      if (newPath !== location.pathname) {
+        navigate(newPath);
+      }
+    }
   };
 
   return (
     <div className="carousel-container">
-      <Slider {...settings} ref={sliderRef}>
-        {slides.map((slide, index) => (
-          <div className="carousel-slide" key={index}>
-            {slide.component}
+      <Slider ref={sliderRef} {...settings}>
+        {slides.map((slide, i) => (
+          <div key={i} className="carousel-slide">
+            <div className="slide-content">{slide.component}</div>
           </div>
         ))}
       </Slider>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -12,7 +12,7 @@ const Projects = () => {
                         <h3 className="previous-work-text">
                         This is a Monster Hunter World companion site that allows users to look up different items found in the game and save them to a favorite list.
                         <a href="https://github.com/schrolan/react-mhw-companion" target="_blank"><h4>GitHub repository.</h4></a>
-                        <a href="http://3.141.0.125:3000/" target="_blank"><h4>And here is the deployed    site. Deployed using AWS.</h4></a>
+                        <a href="http://3.141.0.125:3000/" target="_blank"><h4>Deployed site using aws.</h4></a>
                         </h3>
                         <img src={mhw} alt="a screenshot of my proct react mhw companion." className="float-bottom-left"/>
                     </div>
@@ -20,7 +20,7 @@ const Projects = () => {
                         <h3 className="previous-work-2-text">
                             This is a site for readers that allows a user to create a profile, look up books given a criteria, and save them to a read later list.
                             <a href="https://github.com/schrolan/react-hand-me-a-book" target="_blank"><h4>GitHub repository.</h4></a>
-                            <a href="http://3.22.112.97/" target="_blank"><h4>Here is the deployed site. Once again deployed using AWS.</h4></a>
+                            <a href="http://3.22.112.97/" target="_blank"><h4>Deployed site using aws.</h4></a>
                         </h3>
                             <img src={handmeabook} alt="a screenshot of my project hand me a book." className="float-bottom-left"/>
                     </div>
